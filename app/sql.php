59bc48d20f0691841855a4367184a3ee929b1ac2
nombom7 <nomuken.nom+git@gmail.com>
2019-11-03 13:21:44 +0900

    first commit

---
+++ app/sql.php
@@ -0,0 +1,26 @@
+<?php
+$dsn = 'pgsql:dbname=TEST;host=pgsql;port=5432';
+$user = 'postgres';
+$pass = 'example';
+
+try {
+  // DBに接続する
+  $dbh = new PDO($dsn, $user, $pass);
+
+  // ここでクエリ実行する
+// queryメソッド(SELECT)
+$query_result = $dbh->query('SELECT * FROM test_comments');
+  // DBを切断する
+  $dbh = null;
+} catch (PDOException $e) {
+    // 接続にエラーが発生した場合ここに入る
+    print "DB ERROR: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
+
+<?php
+  foreach($query_result as $row) {
+    print $row["name"] . ": " . $row["text"] . "<br/>";
+  }
+?>
